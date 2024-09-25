34cc908240f7dace713f31477ec19cf680be84fa
aayushwrld <aayush.arora@kalvium.community>
2024-09-25 12:07:47 +0530

    Added Menu Options

--- src/Farm.java
+++ src/Farm.java
@@ -2,32 +2,46 @@ import animals.Cow;
 import animals.Sheep;
 
 public class Farm {
-    public static void main(String[] args) {
-        Cow[] cowHerd = new Cow[3];
+    private Cow[] cowHerd;
+    private Sheep[] sheepHerd;
+
+    public Farm() {
+        cowHerd = new Cow[3];
         cowHerd[0] = new Cow("Bessie", 5, 10.5);
         cowHerd[1] = new Cow("Daisy", 4, 8.2);
         cowHerd[2] = new Cow("Molly", 6, 12.0);
 
-        Sheep[] sheepHerd = new Sheep[2];
+        sheepHerd = new Sheep[2];
         sheepHerd[0] = new Sheep("Lamb", 2, 15.0);
         sheepHerd[1] = new Sheep("Ewe", 3, 20.0);
+    }
 
-//        System.out.println("Cow Herd Details:");
-//        for (Cow cow : cowHerd) {
-//            cow.displayDetails();
-//        }
-//        System.out.println("---------------------------");
-//        System.out.println("Total Cows: " + Cow.getTotalCows());
-//        System.out.println("---------------------------");
-//        System.out.println("Sheep Herd Details:");
-//        for (Sheep sheep : sheepHerd) {
-//            sheep.displayDetails();
-//        }
-//        System.out.println("---------------------------");
-//        System.out.println("Total Sheep: " + Sheep.getTotalSheep());
-//        System.out.println("---------------------------");
+    public void displayTotalAnimals() {
+        int totalSheep = Cow.getTotalCows() + Sheep.getTotalSheep();
+        System.out.println("Total number of animals: " + totalSheep);
+    }
 
-        Cow.displayTotalCows();
-        Sheep.displayTotalSheep();
+    public void displayAnimalDetails() {
+        System.out.println("Cow Herd Details:");
+        for (Cow cow : cowHerd) {
+            cow.displayDetails();
+        }
+        System.out.println("---------------------------");
+        System.out.println("Total Cows: " + Cow.getTotalCows());
+        System.out.println("---------------------------");
+
+        System.out.println("Sheep Herd Details:");
+        for (Sheep sheep : sheepHerd) {
+            sheep.displayDetails();
+        }
+        System.out.println("---------------------------");
+        System.out.println("Total Sheep: " + Sheep.getTotalSheep());
+        System.out.println("---------------------------");
+    }
+
+    public void displayAnimalTypes() {
+        System.out.println("Types of animals on the farm:");
+        System.out.println("1. Cows");
+        System.out.println("2. Sheep");
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,48 @@
+import java.util.Scanner;
+
 public class Main {
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Farm farm = new Farm(); // Create an instance of Farm to access its methods
+
+        while (true) {
+            System.out.println("\nWelcome to the Farm!");
+            System.out.println("Please select an option:");
+            System.out.println("1. View total number of animals");
+            System.out.println("2. View details of all animals");
+            System.out.println("3. View types of animals");
+            System.out.println("4. Exit");
+            System.out.print("Enter your choice (1-4): ");
+
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (choice) {
+                case 1:
+                    farm.displayTotalAnimals();
+                    break;
+                case 2:
+                    farm.displayAnimalDetails();
+                    break;
+                case 3:
+                    farm.displayAnimalTypes();
+                    break;
+                case 4:
+                    System.out.println("Exiting the program. Goodbye!");
+                    scanner.close();
+                    return;
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+                    continue;
+            }
+
+            System.out.print("Would you like to go back to the main menu? (y/n): ");
+            String goBack = scanner.nextLine().trim().toLowerCase();
+            if (!goBack.equals("y")) {
+                System.out.println("Exiting the program. Goodbye!");
+                scanner.close();
+                return;
+            }
+        }
+    }
 }
